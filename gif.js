4980594a18a22d3a7f5449edb7b61827e83dbe3e
JJ <jianan.beijing@gmail.com>
2017-07-31 15:32:23 -0700

    correcting

--- gif.js
+++ gif.js
@@ -1,78 +1,77 @@
-// var topics= [
-// "alexander mcqueen",
-// "beauty",
-// "cara delevingne",
-// "chanel",
-// "christian dior",
-// "dress",
-// "emily ratajkowski",
-// "gucci",
-// "jessica stam",
-// "jewelry",
-// "jourdan dunn",
-// "julia nobis",
-// "karlie kloss",
-// "kate moss",
-// "kate upton",
-// "louis vuitton",
-// "makeup",
-// "marc jacobs",
-// "miranda kerr",
-// ];
+var topics= [
+"alexander mcqueen",
+"beauty",
+"cara delevingne",
+"chanel",
+"christian dior",
+"dress",
+"emily ratajkowski",
+"gucci",
+"jessica stam",
+"jewelry",
+"jourdan dunn",
+"julia nobis",
+"karlie kloss",
+"kate moss",
+"kate upton",
+"louis vuitton",
+"makeup",
+"marc jacobs",
+"miranda kerr",
+];
 
 // function displayGifs() {
-$("button").on("click",function() {
+$(document).on("click",".btn-info",function() {
 
         var topics = $(this).attr("data-topic");
-        var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + topics + "&api_key=37c902b92c944962af80ae11653d4c09";
+        var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + topics + "&api_key=d0fbf00281f04f65b1e0d7399d176bcf";
 
         $.ajax({
           url: queryURL,
           method: "GET"
         }).done(function(response) {
-        	console.log(queryURL);
+        	console.log(queryURL);	
         	console.log(response);
 
-//         var results = response.data;
-
-//         var topicDiv = $("<div>");
+        var results = response.data;
+        for (var i = 0; i < results.length; i++){
         
-//         var p = $("<p>").text("Rating: "+ results[i].rating);
-   
-//         var gif = $("<img>")
-//         gif.attr("src",results[i].images.fixed_width.url)
-       	
-//         topicDiv.append(p);
-//         topicDiv.append(gif);
-
-//         $("#gifs-view").prenpend(topicDiv);
-        });
-
+                var topicDiv = $("<div>");
+                
+                var p = $("<p>").text("Rating: "+ results[i].rating);
+           
+                var gif = $("<img>")
+                gif.attr("src",results[i].images.fixed_height_still.url);
+               	
+                topicDiv.append(p);
+                topicDiv.append(gif);
+        
+                $("#gifs-view").prepend(topicDiv);
+               
+        }
+      });
       });
 
-// function renderButtons() {
-//         $("#buttons-view").empty();
-//         for (var i = 0; i < topics.length; i++) {
+function renderButtons() {
+        $("#buttons-view").empty();
+        for (var i = 0; i < topics.length; i++) {
 
-//           var a = $("<button>");
-//           a.addClass("topic");
-//           a.attr("data-name", topics[i]);
-//           a.text(topics[i]);
-//           $("#buttons-view").append(a);
-//         }
-//       }
+          var a = $("<button>");
+          a.addClass("topic btn-info");
+          a.attr("data-name", topics[i]);
+          a.text(topics[i]);
+          $("#buttons-view").append(a);
+        }
+      }
 
-// $("#add-topic").on("click", function(event) {
-//         event.preventDefault();      
-        
-//         var topic= $("#topic-input").val().trim();
-//         topics.push(topic);
+$("#topics-form").on("submit", function(event) {
+        event.preventDefault();      
+        alert("hiiiii");
+        var topic= $("#topic-input").val().trim();
+        topics.push(topic);
 
-//         renderButtons();
+        renderButtons();
 
-//       });
-
-//       $(document).on("click", ".topic", displayGifs);
-
+      });
      
-//       renderButtons();
+      renderButtons();
