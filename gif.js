9f31add5379c4c3612d298f2a650fb7e5efd699d
JJ <jianan.beijing@gmail.com>
2017-07-30 22:47:27 -0700

    buttons added but api does not work

--- gif.js
+++ gif.js
@@ -1,74 +1,78 @@
-var topics= [
-"alexander mcqueen",
-"beauty",
-"cara delevingne",
-"chanel",
-"christian dior",
-"dress",
-"emily ratajkowski",
-"gucci",
-"jessica stam",
-"jewelry",
-"jourdan dunn",
-"julia nobis",
-"karlie kloss",
-"kate moss",
-"kate upton",
-"louis vuitton",
-"makeup",
-"marc jacobs",
-"miranda kerr",
-"model",
-"prada",
-"runway",
-"shoes",
-"victorias secret"];
+// var topics= [
+// "alexander mcqueen",
+// "beauty",
+// "cara delevingne",
+// "chanel",
+// "christian dior",
+// "dress",
+// "emily ratajkowski",
+// "gucci",
+// "jessica stam",
+// "jewelry",
+// "jourdan dunn",
+// "julia nobis",
+// "karlie kloss",
+// "kate moss",
+// "kate upton",
+// "louis vuitton",
+// "makeup",
+// "marc jacobs",
+// "miranda kerr",
+// ];
 
-function displayGifs() {
+// function displayGifs() {
+$("button").on("click",function() {
 
-        var topic = $(this).attr("data-name");
-        var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + topic + "&rating=&api_key=37c902b92c944962af80ae11653d4c09";
+        var topics = $(this).attr("data-topic");
+        var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + topics + "&api_key=37c902b92c944962af80ae11653d4c09";
 
         $.ajax({
           url: queryURL,
           method: "GET"
         }).done(function(response) {
-        var topicDiv = $("<div class = 'topic'>");
-        var rating = response.rating;
-        var pOne = $("<p>").text("Rating: "+ rating);
-        topiDiv.append(pOne);
-          
-        var imgURL = $("<div ")
-          // Creates an element to hold the image
-          // Appends the image
-          // Puts the entire Movie above the previous movies.
+        	console.log(queryURL);
+        	console.log(response);
 
+//         var results = response.data;
+
+//         var topicDiv = $("<div>");
+        
+//         var p = $("<p>").text("Rating: "+ results[i].rating);
+   
+//         var gif = $("<img>")
+//         gif.attr("src",results[i].images.fixed_width.url)
+       	
+//         topicDiv.append(p);
+//         topicDiv.append(gif);
+
+//         $("#gifs-view").prenpend(topicDiv);
         });
-
-      }
-
-function renderButtons() {
-        $("#buttons-view").empty();
-        for (var i = 0; i < topics.length; i++) {
-
-          var a = $("<button>");
-          a.addClass("topic");
-          a.attr("data-name", topics[i]);
-          a.text(topics[i]);
-          $("#buttons-view").append(a);
-        }
-      }
-
-$("#add-topic").on("click", function(event) {
-        event.preventDefault();      
-        var topic= $("#topic-input").val().trim();
-        topics.push(topic);
-
-        renderButtons();
 
       });
 
-      $(document).on("click", ".topic", displayGifs);
+// function renderButtons() {
+//         $("#buttons-view").empty();
+//         for (var i = 0; i < topics.length; i++) {
+
+//           var a = $("<button>");
+//           a.addClass("topic");
+//           a.attr("data-name", topics[i]);
+//           a.text(topics[i]);
+//           $("#buttons-view").append(a);
+//         }
+//       }
+
+// $("#add-topic").on("click", function(event) {
+//         event.preventDefault();      
+        
+//         var topic= $("#topic-input").val().trim();
+//         topics.push(topic);
+
+//         renderButtons();
+
+//       });
+
+//       $(document).on("click", ".topic", displayGifs);
 
      
-      renderButtons();
+//       renderButtons();
